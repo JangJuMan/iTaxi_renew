bf4495f26f7c5d4802054675ffe3a83ba007cba5
MS-Harine <21700371@handong.edu>
2019-07-17 18:20:52 +0900

    Modify fontsize

--- pages/firstList.js
+++ pages/firstList.js
@@ -5,8 +5,8 @@ import { vw } from 'react-native-expo-viewport-units';
 /**
  * @props   time    Time to leave
  * @props   seats   Number of current seats
- * @props   from    
- * @props   to      
+ * @props   from
+ * @props   to
  * @props   carrier Number of carriers
  */
 export default class List extends Component{
@@ -77,7 +77,7 @@ const styles=StyleSheet.create({
 
             time: {
                 color: 'gray',
-                fontSize: vw(6),
+                fontSize: vw(5),
             },
 
             seat: {
