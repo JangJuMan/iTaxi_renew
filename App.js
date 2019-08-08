35d70ac40c6268fd05104d268822e683642076f6
MS-Harine <21700371@handong.edu>
2019-08-08 12:03:38 +0900

    Change calendar and swiper

--- App.js
+++ App.js
@@ -6,6 +6,8 @@ import TaxiStore from './stores/taxi';
 import UserStore from './stores/user';
 import CarpoolStore from './stores/carpool';
 
+import DateFormat from './src/functions/dateFormat'
+
 const taxiStore = new TaxiStore();
 const userStore = new UserStore();
 const carpoolStore = new CarpoolStore();
