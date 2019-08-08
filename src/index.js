3ede0f0ff056f833b7c1fb5987eb8b90d37fc651
gksksla <21900398@handong.edu>
2019-08-08 14:32:46 +0900

    가운데정렬_헤더

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
 import { View, Text, Button } from 'react-native';
-import { createBottomTabNavigator, createAppContainer, createStackNavigator, createMaterialTopTabNavigator } from 'react-navigation';
+import { createBottomTabNavigator, createAppContainer, createStackNavigator, createMaterialTopTabNavigator} from 'react-navigation';
 import Icon from 'react-native-vector-icons/FontAwesome5';
-
 import ListView from './pages/taxiList';
 import RiderLog from './pages/rideLog';
 import Setting from './pages/settingscreen';
@@ -17,6 +16,7 @@ const TaxiTab = createStackNavigator({
     // TaxiRoom: ChatRoom,
     // createTaxiRoomInfo: Setting,
   }, {
+  headerLayoutPreset:'center',
   defaultNavigationOptions: {
     title: "조회 / 모집",
   }
@@ -25,12 +25,13 @@ const TaxiTab = createStackNavigator({
 const CarpoolTab = createStackNavigator({
     Home: Carpool,
     CarpoolRoomDoor: EnteringRoom,
+
     // CarpoolRoom: ChatRoom,
     // createCarpoolRoomInfo: Setting,
   }, {
-  defaultNavigationOptions: {
-    title: "조회 / 모집",
-    // headerTitleStyle:{flexDirection:'column', justifyContent:'center', alignItems:'center',  color:'blue', borderWidth:1},
+    headerLayoutPreset:'center',
+    defaultNavigationOptions:{
+      title: "조회 / 모집",
   }
 });
 
@@ -38,8 +39,9 @@ const RideHistoryTab = createStackNavigator({
     Home: RiderLog,
     // pastRoom: ChatRoom,
   }, {
-  defaultNavigationOptions: {
-    title: "탑승 내역",
+    headerLayoutPreset:'center',
+    defaultNavigationOptions: {
+      title: "탑승 내역",
   }
 });
 
@@ -47,6 +49,7 @@ const MyPageTab = createStackNavigator({
     Home: Setting,
     Update: UpdateInfo,
   }, {
+  headerLayoutPreset:'center',
   defaultNavigationOptions: {
     title: "설정",
   }
@@ -112,6 +115,7 @@ const MyPageTab = createStackNavigator({
 
 // 새로운 것
 const TabNavigator = createMaterialTopTabNavigator({
+
   TaxiList: {
     screen: TaxiTab,
     navigationOptions: {
@@ -140,6 +144,7 @@ const TabNavigator = createMaterialTopTabNavigator({
 }, 
 {
   initialRouteName: 'RideHistory',
+  
   defaultNavigationOptions: ({ navigation }) => ({
     tabBarIcon: ({ focused, horizontal, tintColor }) => {
       const { routeName } = navigation.state;
