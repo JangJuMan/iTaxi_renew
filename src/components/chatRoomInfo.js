8f63ca3d652a4a748884a8e15cf98070f53edf39
gksksla <21900398@handong.edu>
2019-08-08 16:55:03 +0900

    change chatroominfo

--- src/components/chatRoomInfo.js
+++ src/components/chatRoomInfo.js
@@ -25,88 +25,53 @@ export default class ChatRoom extends Component{
         const t_data = taxiStore.taxiId;
         const c_data = carpoolStore.carpoolId;
 
-        // <View style={styles.calendar}>
-        //                     <Icon name="calendar" color='#3FA9F5' size={20}></Icon>
-        //                     <Text>   2019 / 07 / 31</Text>
-        //                 </View>
+       
 
 
-        // <View style={styles.clock}>
-        //                     <Icon name="clockcircleo" color='#3FA9F5' size={20}></Icon>
-        //                     <Text>  {t_data.departure_time.substring(7)}</Text>
-        //                 </View>
 
-
-    //     <View style={styles.destination_location}>
-    //     <Image
-    //         style={styles.destination_image}
-    //         source={fromtoIcon} />
-    //     <View style={styles.destination_text_location}>
-    //         <Text style={styles.destination_text}>
-    //             {t_data.departure_place}
-    //         </Text>
-    //         <Text style={styles.destination_text}>
-    //             {t_data.arrival_place}
-    //         </Text>
-    //     </View>
-    // </View>
 
     
-    // <View style={styles.chat_leftButt_carrier}>
-    //                         <View style={{flexDirection:'row',padding:3}}>
-    //                             <Image style={styles.carrIcon} source={carrIcon} />
-    //                             <Text style={{padding:5}}>캐리어</Text>
-    //                         </View>
-    //                         <View style={{flexDirection:'row',margin:5}}>
-    //                             <Image style={styles.carrImg} source={emptycarrImg} />
-    //                             <Image style={styles.carrImg} source={emptycarrImg} />
-    //                             <Image style={styles.carrImg} source={fullcarrImg} />
-    //                             <Image style={styles.carrImg} source={fullcarrImg} />
-    //                         </View>
-    //                     </View>
 
 
         return(
         <View>
             <View style={styles.chatInfo_Top}>
                 <View style={styles.chatInfo_Left}>
-                    <View style={styles.chat_leftTop}>
+                    <View style={styles.calendar_time}>
                         <View style={styles.calendar}>
                             <Icon name="calendar" color='#3FA9F5' size={20}></Icon>
-                            <Text>   2019 / 07 / 31</Text>
+                            <Text style={{fontSize:15}}>  07 / 31</Text>
                         </View>
                         <View style={styles.clock}>
                             <Icon name="clockcircleo" color='#3FA9F5' size={20}></Icon>
-                            <Text>  {t_data.departure_time.substring(7)}</Text>
+                            <Text style={{fontSize:15}}>  {t_data.departure_time}</Text>
                         </View>
                     </View>
-                    <View style={styles.chat_leftButt}>
-                        <View style={styles.fromto}>
-                            <View style={styles.destination_location}>
-                                <Image
-                                    style={styles.destination_image}
-                                    source={fromtoIcon} />
-                                <View style={styles.destination_text_location}>
-                                    <Text style={styles.destination_text}>
-                                        {t_data.departure_place}
-                                    </Text>
-                                    <Text style={styles.destination_text}>
-                                        {t_data.arrival_place}
-                                    </Text>
-                                </View>
+                    <View style={styles.destination}>
+                        <View style={styles.destination_location}>
+                            <Image
+                                style={styles.destination_image}
+                                source={fromtoIcon} />
+                            <View style={styles.destination_text_location}>
+                                <Text style={styles.destination_text}>
+                                    {t_data.departure_place}
+                                </Text>
+                                <Text style={styles.destination_text}>
+                                    {t_data.arrival_place}
+                                </Text>
                             </View>
                         </View>
-                        <View style={styles.chat_leftButt_carrier}>
-                            <View style={{flexDirection:'row',padding:3}}>
-                                <Image style={styles.carrIcon} source={carrIcon} />
-                                <Text style={{padding:5}}>캐리어</Text>
-                            </View>
-                            <View style={{flexDirection:'row',margin:5}}>
-                                <Image style={styles.carrImg} source={emptycarrImg} />
-                                <Image style={styles.carrImg} source={emptycarrImg} />
-                                <Image style={styles.carrImg} source={fullcarrImg} />
-                                <Image style={styles.carrImg} source={fullcarrImg} />
-                            </View>
+                    </View>
+                    <View style={styles.carrier}>
+                        <View style={{flexDirection:'row',padding:3}}>
+                            <Image style={styles.carrIcon} source={carrIcon} />
+                            <Text style={{padding:5}}>캐리어</Text>
+                        </View>
+                        <View style={{flexDirection:'row',margin:5}}>
+                            <Image style={styles.carrImg} source={emptycarrImg} />
+                            <Image style={styles.carrImg} source={emptycarrImg} />
+                            <Image style={styles.carrImg} source={fullcarrImg} />
+                            <Image style={styles.carrImg} source={fullcarrImg} />
                         </View>
                     </View>
                 </View>
@@ -170,77 +135,75 @@ const styles=StyleSheet.create({
         flexDirection:'row',
     },
         chatInfo_Left:{
-            flex:3,
+            flex:5,
             borderColor:'gray',
             borderWidth:0.5,
+            flexDirection:'row',
         },
-            chat_leftTop:{
-                flex:1,
-                flexDirection:'row',
-                justifyContent:'center',
+            calendar_time:{
+                flex:2,
+                flexDirection:'column',
+                margin:5,
+                paddingLeft:7,
+                justifyContent:'space-evenly',
                 alignItems:'center',
             },
                 calendar:{
-                    flex:7,
                     flexDirection:'row',
-                    justifyContent:'center'
                 },
                 clock:{
-                    flex:4,
                     flexDirection:'row',
-                    justifyContent:'flex-start'
                 },
-            chat_leftButt:{
-                flex:2,
-                flexDirection:'row'
+                
+            destination:{
+                flex:4,
             },
-                fromto:{
-                    flex:1,
+                destination_location: {
+                    flex: 3,
+                    marginLeft: vw(3),
+                    justifyContent: 'center',
+                    flexDirection: 'row',
                 },
-                    destination_location: {
-                        flex: 3,
-                        marginLeft: vw(3),
-                        justifyContent: 'center',
-                        flexDirection: 'row',
+        
+                    destination_image: {
+                        width: vw(6),
+                        height: vw(30),
+                        margin:5,
                     },
-            
-                        destination_image: {
-                            width: vw(6),
-                            height: vw(22),
+        
+                    destination_text_location: {
+                        flex: 1,
+                        flexDirection: 'column',
+                        justifyContent: 'space-evenly',
+                    },
+        
+                        destination_text: {
+                            color: 'gray',
+                            flexDirection:'row'
                         },
-            
-                        destination_text_location: {
-                            flex: 1,
-                            flexDirection: 'column',
-                            justifyContent: 'space-evenly',
-                        },
-            
-                            destination_text: {
-                                color: 'gray',
-                                margin: vw(2.4),
-                            },
-                chat_leftButt_carrier:{
-                    flex:1,
-                    flexDirection:'column',
+            carrier:{
+                flex:3,
+                flexDirection:'column',
+                justifyContent:'center',
+                alignItems:'center'
+            },
+                carrIcon:{
+                    width: vw(7),
+                    height: vw(7),
                 },
-                    carrIcon:{
-                        width: vw(7),
-                        height: vw(7),
-                    },
-                    carrImg:{
-                        width: vw(5),
-                        height: vw(10),
-                        padding:10
-                    },
+                carrImg:{
+                    width: vw(5),
+                    height: vw(10),
+                },
         chatInfo_Right:{
-            flex:1.5,
+            flex:2,
             flexDirection:'column',
             borderColor:'gray',
             borderWidth:0.5,
             justifyContent:'space-around'
         },
             Button:{
-                height:vw(12),
+                height:vw(10),
                 justifyContent:'center',
                 alignItems:'center',
             },
