f7c88caadcc41573625539bf9d8388097b2625ab
Jina Choi <jiina_28@naver.com>
2019-08-14 16:03:39 +0900

    nothing

--- src/pages/setting/notice.js
+++ src/pages/setting/notice.js
@@ -1,231 +1,70 @@
 import React, { Component } from 'react';
-import { View, Text,StyleSheet,Image,ScrollView,AppRegistry,Button,Alert,Switch} from 'react-native';
-import { TouchableOpacity } from 'react-native-gesture-handler';
+import { AppRegistry, StyleSheet, FlatList, Text, View, Alert, TouchableOpacity } from 'react-native';
+import { ListItem } from 'react-native-elements';
 import { vw } from 'react-native-expo-viewport-units';
 import { observer, inject } from 'mobx-react';
-import titleFont from '../variable/assets';
+
+export default class Setting extends Component {
+
+    render() {
+        const list = [
+            {
+                subtitle: '2019-08-14',
+                title: '아이택시가 새로 바뀌었어요!',
+            },
+            {
+                subtitle: '2019-08-13',
+                title: '영현이가 바보래요',
+            },
+            {
+                subtitle: '2019-08-11',
+                title: '영현이 짱~',
+            },
+            {
+                subtitle: '2019-08-10',
+                title: '안녕하세요',
+            },
+            {
+                subtitle: '2019-08-09',
+                title: '사랑해요',
+            },
+        ]
 
 
-class Profile_info extends Component {
-  render() {
-    return (
-      <View style={{flex:3,justifyContent:'center',alignItems:'center',}}>
-        <Text style={{fontSize:16,color:'#999999',fontFamily:titleFont,fontWeight:"200"}}>{this.props.info} {'\n'}</Text>
-      </View>
-    );
-  }
-}
-
-class Profile_my extends Component {
-  render() {
-    return (
-      <View style={{flex:3,justifyContent:'center',alignItems:'center',}}>
-        <Text style={{fontSize:16,color:'black',fontFamily:titleFont,fontWeight:"200"}}>{this.props.info} {'\n'}</Text>
-      </View>
-    );
-  }
-}
-
-@inject('userStore')
-@observer
-export default class Setting extends Component{
-  constructor(props){
-    super(props);
-}
-  state={chattingSwitchValue:true}
-  CHATtoggleSwitch=(Chatvalue)=>{
-    this.setState({ChattingSwitchValue: Chatvalue})
-  }
-
-  state={switchValue:true}
-  toggleSwitch=(value)=>{
-    this.setState({switchValue: value})
-  }
-  componentDidMount() {
-    const { userStore } = this.props;
-    userStore.getUserId();
-  }
-  
-    render(){ 
-      const { userStore } = this.props; 
-      const data = userStore.userId;
-        return(
-          
-          <ScrollView>
-                <View style={styles.profileTop}>
-                    <Text style={{fontSize:15,color:'#3FA9F5',fontFamily:titleFont,fontWeight:"200"}}>Profile</Text>
-                    <View style={{borderBottomWidth:1,margin:9, borderBottomColor: '#CCCCCC', flexGrow: 1}}></View>
-                </View>
-                <View style={styles.profileBottom}>
-                    <View style={{flex:3}}>
-                      <Profile_info info='학번'/>
-                      <Profile_info info='한글이름'/>
-                      <Profile_info info='Email'/>
-                      <Profile_info info='휴대폰 번호'/>
-                      <Profile_info info='계좌 은행'/>
-                      <Profile_info info='계좌 번호'/>
-                    </View>
-                    
-                    <View style={{flex:5}}>
-                      <Profile_my info={data.student_id}/>
-                      <Profile_my info={data.name}/>
-                      <Profile_my info={data.student_id + '@handong.edu'}/>
-                      <Profile_my info={data.phone}/>
-                      <Profile_my info='기업'/>
-                      <Profile_my info={data.phone}/>
-                    </View>
-                    
-                </View>
-                <View style={styles.profileButton}>
-                  <TouchableOpacity onPress={() => {
-                    this.props.navigation.navigate('Update', 
-                      {
-                        student_id: data.student_id,
-                        // name: data.name,
-                        // user_id: data.user_id,
-                        // phone: data.phone
-                    })
-                  }}>
-                    <View style={styles.changeInfo}>
-                      <Text style={styles.changeInfoText}>개인정보 수정</Text>
-                    </View>
-                  </TouchableOpacity>
-                </View>
-                <View style={styles.AlarmTop}>
-                    <Text style={{fontSize:15,color:'#3FA9F5',fontFamily:titleFont,fontWeight:"200"}}>Alarm</Text>
-                    <View style={{borderBottomWidth:1,margin:9, borderBottomColor: '#CCCCCC', flexGrow: 1,}}></View>
-                </View>
-                <View style={styles.AlarmBottom}>
-                    <View style={styles.chatAlarm}>
-                      <Text style={{fontSize:16,color:'black',fontFamily:titleFont,fontWeight:"200"}}>채팅 알람</Text>
-                      {/* switch버튼 */}
-                      <View style={styles.switch}>
-                        <Switch
-                          onValueChange = {this.CHATtoggleSwitch}
-                          value = {this.state.ChattingSwitchValue}/>
-                      </View>
-                    </View>
-                    <View style={styles.alertAlarm}>
-                      <Text style={{fontSize:16,color:'black',fontFamily:titleFont,fontWeight:"200"}}>공지 알람</Text>
-                      {/* switch버튼 */}
-                      <View style={styles.switch}>
-                        <Switch
-                          onValueChange = {this.toggleSwitch}
-                          value = {this.state.switchValue}/>
-                      </View>
-                    </View>
-                </View>
-                <View style={styles.versionTop}>
-                    <Text style={{fontSize:15,color:'#3FA9F5',fontFamily:titleFont,fontWeight:"200"}}>Version</Text>
-                    <View style={{borderBottomWidth:1,margin:9, borderBottomColor: '#CCCCCC', flexGrow: 1,}}></View>
-                </View>
-                <View style={styles.versionBottom}>
-                  <Text style={{fontSize:16,color:'black',fontFamily:titleFont,fontWeight:"200"}}>2.0.0-alpha</Text>
-                </View>
-                <View style={styles.versionTop}>
-                    <Text style={{fontSize:15,color:'#3FA9F5',fontFamily:titleFont,fontWeight:"200"}}>Contact Us(버그신고및 문의)</Text>
-                    <View style={{borderBottomWidth:1,margin:9, borderBottomColor: '#CCCCCC', flexGrow: 1,}}></View>
-                </View>
-                <View style={styles.versionBottom}>
-                  <Text style={{fontSize:16,color:'black',fontFamily:titleFont,fontWeight:"200"}}>cra.handong.service@gmail.com</Text>
-                </View>
-          </ScrollView>
+        return (
+            <View style={styles.contain}>
+                {
+                    list.map((item, i) => (
+                        <ListItem
+                            style={styles.line}
+                            Component={TouchableOpacity}
+                            key={i}
+                            subtitle={
+                                <View>
+                                    <Text style={{ color: '#999999', fontSize: vw(4) }}>{item.subtitle}</Text>
+                                </View>
+                            }
+                            title={item.title}
+                            chevron
+                        />
+                    ))
+                }
+            </View>
         )
-  
-  
-    }
-  }
-  
-  
 
-  const styles=StyleSheet.create({
-    changeInfo: {
-      borderRadius: 30,
-      borderWidth: 1,
-      borderColor: '#3FA9F5',
-      paddingLeft: 10,
-      paddingRight: 10,
-      flex: 0.9,
-      flexDirection: 'row',
-      justifyContent: 'space-evenly',
-      alignItems: 'center',
-  },
-      changeInfoText: {
-          color: '#3FA9F5',
-          fontFamily:titleFont,
-          fontWeight:"200"
-      },
-      switch: {
-        justifyContent: 'center',
-        alignItems: 'center',
-      },
-    profileTop:{
-      flexDirection:'row',
-      marginLeft:10,
-      marginTop:10,
-      height:vw(5), 
-      alignItems:'flex-start'
-    },
-    profileBottom:{
-      flexDirection:'row',
-      marginTop:20,
-      height:vw(80),
-      justifyContent:'center',
-      alignContent:'center'
-    },
-    profileButton:{
-      height:vw(15),
-      justifyContent:'center',
-      alignItems:'center'
-    },
-    AlarmTop:{
-      height:vw(5),
-      marginLeft:10,
-      flexDirection:'row',
-      alignItems:'flex-start'
-    },
-    AlarmBottom:{
-      flexDirection:'column',
-      marginTop:20,height:vw(20),
-      justifyContent:'center',
-      alignContent:'center'
-    },
-    chatAlarm:{
-      flex:1,
-      justifyContent:'space-between',
-      paddingHorizontal:50,
-      flexDirection:'row'
-    },
-    alertAlarm:{
-      flex:1,
-      justifyContent:'space-between',
-      paddingHorizontal:50,
-      flexDirection:'row'
-    },
-    versionTop:{
-      height:vw(5),
-      marginLeft:10,
-      flexDirection:'row',
-      alignItems:'flex-start'
-    },
-    versionBottom:{
-      flexDirection:'row',
-      marginTop:20,
-      height:vw(10),
-      justifyContent:'center',
-      alignContent:'center'
-    },
-    contactTop:{
-      height:vw(5),
-      marginLeft:10,
-      flexDirection:'row',
-      alignItems:'flex-start'
-    },
-    contactBottom:{
-      flexDirection:'row',
-      marginTop:20,
-      height:vw(10),
-      justifyContent:'center',
-      alignContent:'center'
+
     }
-    })
+}
+
+
+
+const styles = StyleSheet.create({
+    contain: {
+
+    },
+    line: {
+        borderBottomWidth: 0.5,
+        borderBottomColor: 'gray'
+    },
+})
+
