1b9c087600fbfe48aada798a2183445f6f77369c
Jina Choi <jiina_28@naver.com>
2019-08-15 13:55:32 +0900

    notice content

--- src/pages/setting/detailNotice.js
+++ src/pages/setting/detailNotice.js
@@ -11,9 +11,10 @@ export default class version extends Component {
         super(props);
     }
     render() { 
-        const {navigation}=this.props;
-        const date= navigation.getParam('date','');
-        const title =navigation.getParam('title','');
+        const {navigation} = this.props;
+        const date = navigation.getParam('date','');
+        const title = navigation.getParam('title','');
+        const content = navigation.getParam('content','');
         return (
 
             <View style={{ marginTop: vw(2) }}>
@@ -25,7 +26,7 @@ export default class version extends Component {
                     <Text style={{ fontSize:vw(6), color: 'black', fontFamily: titleFont, fontWeight: "200" }}>#{title}</Text>
                 </View>
                 <View style={styles.box}>
-                    <Text style={{ fontSize:vw(5), color: '#999999', fontFamily: titleFont, fontWeight: "200",textAlign:'center' }}>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</Text>
+                    <Text style={{ fontSize:vw(5), color: '#999999', fontFamily: titleFont, fontWeight: "200",textAlign:'center' }}>{content}</Text>
                 </View>
 
             </View>
